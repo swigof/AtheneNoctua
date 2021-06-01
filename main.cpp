770dab59200c9633e4903347e4eac6c0e9931240
SwigOf <33262954+SwigOf@users.noreply.github.com>
2021-06-01 18:29:16 -0400

    Initial setup

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <windows.h>
 #include <iostream>
 #include <tlhelp32.h>
-#include <TitanEngine.h>
 
 std::wstring wsProcessName = L"MapleLegends.exe";
+DWORD dwPID = 0;
+DWORD dwTID = 0;
+
 DWORD dwListingInfoBreakpoint = 0x60EBDD; // listing count at [ESI] and price at [ESI+8]
 DWORD dwItemInfoBreakpoint = 0x4B5A78; // item ID in EAX and on top of stack
 DWORD dwShopOpenStartEndBreakpoint = 0x5C04BF; // executed once at shop open start then once at shop open end
@@ -65,15 +67,25 @@ bool EnableDebugPrivileges() {
     return true;
 }
 
+void SetBreakpoint(HANDLE hThread, DWORD address) {
+
+}
+
+void SetContext(HANDLE hThread) {
+
+}
+
+
 int main() {
     HANDLE hProcess = OpenProcessByName();
     if (hProcess == nullptr) {
         std::cout << "PROCESS NOT FOUND" << "\n";
         return 0;
     }
+    dwPID = GetProcessId(hProcess);
+    CloseHandle(hProcess);
 
-    DWORD dwPID = GetProcessId(hProcess);
-    DWORD dwTID = GetProcessMainThreadID(dwPID);
+    dwTID = GetProcessMainThreadID(dwPID);
     if (dwTID == 0) {
         std::cout << "MAIN THREAD ID NOT FOUND" << "\n";
         return 0;
@@ -90,15 +102,60 @@ int main() {
         return 0;
     }
 
-    // TODO
-    //AttachDebugger
-    //DebugLoop
-    //GetContextData
-    //SetContextData
-    
-    //SetHardwareBreakPoint
+    CONTEXT ctx = { 0 };
+    ctx.ContextFlags = CONTEXT_ALL;
+    DEBUG_EVENT dbgEvent = { 0 };
+    DWORD dbgStatus = DBG_CONTINUE;
 
+    DebugActiveProcess(dwPID);
+    
+    if (!DebugSetProcessKillOnExit(false)) {
+        std::cout << "FAILED TO SET KILL ON EXIT " << GetLastError() << "\n";
+        return 0;
+    }
+
+    GetThreadContext(hThread, &ctx);
+    ctx.Dr7 |= 1;
+    ctx.Dr0 = dwListingInfoBreakpoint;
+    SetThreadContext(hThread, &ctx);
+
+    while(true) {
+        WaitForDebugEvent(&dbgEvent, INFINITE);
+        dbgStatus = DBG_CONTINUE;
+        std::cout << dbgEvent.dwDebugEventCode << "\n";
+        switch (dbgEvent.dwDebugEventCode) {
+        case EXCEPTION_DEBUG_EVENT:
+            switch (dbgEvent.u.Exception.ExceptionRecord.ExceptionCode) {
+                case EXCEPTION_BREAKPOINT:
+                    std::cout << "breakpoint" << "\n";
+                    GetThreadContext(hThread, &ctx);
+                    ctx.Dr7 = 0;
+                    ctx.EFlags |= (1 << 8);
+                    SetThreadContext(hThread, &ctx);
+                    break;
+                case EXCEPTION_SINGLE_STEP:
+                    std::cout << "single step" << "\n";
+                    GetThreadContext(hThread, &ctx);
+                    ctx.EFlags &= ~(1 << 8);
+                    SetThreadContext(hThread, &ctx);
+                    break;
+                default:
+                    std::cout << "other exception" << "\n";
+                    dbgStatus = DBG_EXCEPTION_NOT_HANDLED;
+                    break;
+            }
+        default:
+            dbgStatus = DBG_EXCEPTION_NOT_HANDLED;
+            break;
+        }
+        ContinueDebugEvent(dbgEvent.dwProcessId, dbgEvent.dwThreadId, dbgStatus);
+    }
+
+    GetThreadContext(hThread, &ctx);
+    ctx.Dr7 = 0;
+    SetThreadContext(hThread, &ctx);
+
+    DebugActiveProcessStop(dwPID);
     CloseHandle(hThread);
-    CloseHandle(hProcess);
     return 0;
 }
