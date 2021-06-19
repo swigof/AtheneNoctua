af86a223c69989403e0faa254fe49f9fd790b4d5
SwigOf <33262954+SwigOf@users.noreply.github.com>
2021-06-19 16:27:09 -0400

    added channel and map instructions

--- AtheneNoctua.h
+++ AtheneNoctua.h
@@ -3,14 +3,24 @@
 // hash check for client version?
 // scan for these instead of static? take down instruction values to refind
 
-// need shop names (and/or ids?) on open 
-// need player map and channel
-// list of shop names/ids on map enter to check which have dissapeared?
+// in order on any map/channel change
+#define CHANNEL_CHANGE_INSTRUCTION 0x65BC28 // channel-1 in EAX
+#define MAP_CHANGE_INSTRUCTION 0x6DBC3A     // map ID in EAX 
+
+// in order on shop open
 #define SHOP_OPEN_INSTRUCTION 0x5C04BF		// executed once on initial shop open and once when the shop has loaded
-#define LISTING_INFO_INSTRUCTION 0x60EBDA	// listing count at [ESI] and price at [ESI+8]
-#define ITEM_INFO_INSTRUCTION 0x4B5A78		// item ID in EAX and on top of stack
+#define LISTING_INFO_INSTRUCTION 0x60EBDA	// listing count at [ESI] and price at [ESI+8] (change to find when each in register?)
+#define ITEM_INFO_INSTRUCTION 0x4B5A78		// item ID in EAX (executes at other times as well not sure specifically what triggers it)
 
 #define STATUS_WX86_SINGLE_STEP 0x4000001E
-#define STATUS_WX86_BREAKPOINT 0x4000001F
 
 void StartTools();
+
+//id shop open
+// 
+//shop name (or player name?) open
+// 
+//map load shop ids, to check which have dissapeared
+//
+//full item details including stats and all
+//
